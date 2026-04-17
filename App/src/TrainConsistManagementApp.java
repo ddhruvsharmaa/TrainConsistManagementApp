e1d45bea94d5e41c30410bff61c932ad3598c61e
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Pro.local>
2026-04-18 01:50:21 +0530

    UC13: Performance Comparison

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,27 +1,21 @@
 import java.util.*;
 
-class Bogie {
-    String type;
-    int load;
-
-    Bogie(String type, int load) {
-        this.type = type;
-        this.load = load;
-    }
-}
-
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
-        List<Bogie> list = Arrays.asList(
-                new Bogie("Goods", 500),
-                new Bogie("Goods", 1200)
-        );
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) list.add(i);
 
-        for (Bogie b : list) {
-            if (b.type.equals("Goods") && b.load > 1000) {
-                System.out.println("Unsafe Bogie");
-            }
-        }
+        long start1 = System.nanoTime();
+        int sum1 = 0;
+        for (int i : list) sum1 += i;
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        int sum2 = list.stream().reduce(0, Integer::sum);
+        long end2 = System.nanoTime();
+
+        System.out.println("Loop Time: " + (end1 - start1));
+        System.out.println("Stream Time: " + (end2 - start2));
     }
 }
