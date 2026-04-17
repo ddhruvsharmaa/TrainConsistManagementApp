94cadd9f4c01505b0af5f12d7e9f4d66ffdb40ca
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Pro.local>
2026-04-18 01:58:28 +0530

    UC14: Handle Invalid Capacity

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,21 +1,14 @@
-import java.util.*;
-
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 100000; i++) list.add(i);
+        int capacity = -10;
 
-        long start1 = System.nanoTime();
-        int sum1 = 0;
-        for (int i : list) sum1 += i;
-        long end1 = System.nanoTime();
-
-        long start2 = System.nanoTime();
-        int sum2 = list.stream().reduce(0, Integer::sum);
-        long end2 = System.nanoTime();
-
-        System.out.println("Loop Time: " + (end1 - start1));
-        System.out.println("Stream Time: " + (end2 - start2));
+        try {
+            if (capacity < 0) {
+                throw new IllegalArgumentException("Invalid Capacity");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
