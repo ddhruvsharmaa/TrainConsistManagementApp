cab9a0e34c2a7f329d0c89c8f4833503e65bfdd6
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Pro.local>
2026-04-18 00:43:28 +0530

    UC1: Initialize Train

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,13 @@
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Train Initialized");
+
+        String trainName = "Express-101";
+        int totalBogies = 0;
+
+        System.out.println("Train Name: " + trainName);
+        System.out.println("Total Bogies: " + totalBogies);
+    }
+}
